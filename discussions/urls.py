ffc25f1effeda707c8fd8f7edca004c33d2182fc
haeuniea <haeunhaa@gmail.com>
2026-01-05 15:56:47 +0900

    [feat]discussions 게시글 작성 기능 추가(임시작성자 적용)

--- discussions/urls.py
+++ discussions/urls.py
@@ -6,4 +6,5 @@ app_name = 'discussions'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:post_id>/',views.post_detail,name='post_detail'),
+    path('create/',views.post_create,name='post_create'),
 ]
--- discussions/views.py
+++ discussions/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import DiscussionPost
+from django.contrib.auth import get_user_model # 임시
+# from django.contrib.auth.decorators import login_required
+User = get_user_model() # 임시
 
 # Create your views here.
 def post_list(request):
@@ -17,3 +20,22 @@ def post_list(request):
 def post_detail(request, post_id):
     post = get_object_or_404(DiscussionPost, id=post_id)
     return render(request,'discussions/post_detail.html',{'post':post})
+
+# @login_required
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        link = request.POST.get('link')
+        image = request.FILES.get('image')
+
+        DiscussionPost.objects.create(
+            author=User.objects.first(), # 임시 -> author=request.user
+            title=title,
+            content=content,
+            link=link,
+            image=image
+        )
+        return redirect('discussions:post_list')
+    
+    return render(request,'discussions/post_form.html')
