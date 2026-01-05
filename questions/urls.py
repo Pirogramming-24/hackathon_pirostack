818879c2c9948764d6be31e09271b8dd4fa67575
Youngju Lee <oortmealy@gmail.com>
2026-01-05 21:23:22 +0900

    [add] : urls.py에 새 경로 추가

--- questions/urls.py
+++ questions/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('<int:pk>/', views.question_detail, name='detail'),
     path('create/', views.question_create, name='create'),
     path('<int:pk>/answer/', views.answer_create, name='answer_create'),
+    path('<int:pk>/answer/<int:answer_pk>/reply/', views.reply_create, name='reply_create'),
     path('<int:pk>/resolve/', views.question_resolve, name='resolve'),
+    path('<int:pk>/scrap/', views.question_scrap, name='scrap'),
+    path('<int:pk>/update/', views.question_update, name='update'),
+    path('<int:pk>/delete/', views.question_delete, name='delete'),
     path('staff/', views.staff_unanswered, name='staff_unanswered'),
 ]
