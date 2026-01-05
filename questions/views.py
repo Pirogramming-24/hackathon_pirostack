5a2d7e02bdcf2d15851d4b7c4e43741935d53a6e
mcaaa9669 <rimiof9669@naver.com>
2026-01-05 13:47:57 +0900

    [feat]질문 필터링

--- questions/views.py
+++ questions/views.py
@@ -9,29 +9,68 @@ from .forms import QuestionForm, AnswerForm
 def question_list(request):
     """전체 질문 목록"""
     questions = Question.objects.all()
+    sort_param = request.GET.get("sort", "latest")
+
+    if sort_param =="oldest":
+        questions = questions.order_by("created_at", "id")
+    else:
+        questions = questions.order_by("-created_at", "-id")
     categories = Category.objects.all()
     context = {
         "questions": questions,
         "categories": categories,
+        "selected_sort": sort_param,
+        "selected_category":None,
     }
     return render(request, "questions/question_list.html", context)
 
 
 def question_list_by_category(request, category_id):
+
+
     """
     [이서현] 카테고리별, 과제별, 시간순 정렬 및 필터링
     TODO: 커스텀 정렬 API 및 필터링 뷰 구현
     """
+
     # 에러 방지를 위한 기본 구현
-    category = get_object_or_404(Category, pk=category_id)
-    questions = Question.objects.filter(category=category)
+    category = get_object_or_404(Category, pk=category_id)  
+    questions = Question.objects.filter(category=category)    
+
+    #화면 렌더링에 필요한 카테고리 목록
     categories = Category.objects.all()
+    sort_param = request.GET.get("sort", "latest") #기본값 lagtest로 설정
+    session_categories = Category.objects.filter(category_type="session").order_by("name")
+    assignment_categories = Category.objects.filter(category_type="assignment").order_by("name")
+
+
+ 
+    
+    #시간순 정렬 
+    if sort_param == "oldest":
+        #오래된 순
+        questions = questions.order_by("created_at")
+    else:
+        #최신순
+        questions = questions.order_by("-created_at")
+
+    
+    #템플릿에 전달할 데이터 꾸러미(context)
     context = {
         "questions": questions,
         "categories": categories,
         "selected_category": category,
+
+        #현재 선택된 필터 상태도 같이 넘겨두면 템플릿에서 선택값 유지하기 용이함
+        "session_categories": session_categories,
+        "assignment_categories": assignment_categories,
+        "selected_sort": sort_param,
     }
+
+    #질문 리스트 페이지 렌더링
     return render(request, "questions/question_list.html", context)
+
+
 
 
 def question_detail(request, pk):
