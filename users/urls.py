8b5a686ac9071d6fa5cdbf218f15faf4c62ce94a
Youngju Lee <oortmealy@gmail.com>
2026-01-06 12:01:23 +0900

    [add] : 로그아웃 토글 추가

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup/', views.signUp, name='signUp'),
     path('',views.login, name='login'),
     path('signupPass/<int:pk>/',views.signupPass,name='signupPass'),
-    path('loginPass/<int:pk>/',views.loginPass,name='loginPass')
+    path('loginPass/<int:pk>/',views.loginPass,name='loginPass'),
+    path('logout/', views.logout_view, name='logout')
 ]
